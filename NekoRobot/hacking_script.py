e4411c56dd6bab9a84475eaa07f5abdaaf51c408
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-23 02:49:17 +0530

    Update hacking_script.py

--- NekoRobot/hacking_script.py
+++ NekoRobot/hacking_script.py
@@ -1,14 +1,14 @@
 PM_START_TEXT = """
-────「 [{}](https://telegra.ph/file/bdc771a084e2c806ec756.jpg) 」────
-*やあ! {} My Darling, Iam your SweetHeart💜,
-I am an Anime themed advance group management bot with a lot of Sexy Features.*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
+────「 [{}](https://telegra.ph/file/586a4636cca3f46c6cc74.jpg) 」────
+*やあ! {} My Darling, Iam your SweetHeart❤️
+I am a Hentai themed advance Group Management bot with a lot of Sexy Features.*
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 ◈ *Uptime:* `{}`
 ◈ `{}` *users, across* `{}` *chats.*
 ◈ *Thanks To @NekoXRobot For NekoRobot Repo*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 ➢ Try The Help Buttons Below To Know My Abilities ♡
 """
 
 
-## Don't Remove That Credits in PM_START_TEXT if yoy removed your app will be crashed.
+## Don't Remove That Credits in PM_START_TEXT if you removed your app will be crashed.
