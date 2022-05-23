573db4d46dbe16c32aca7ed2a34e5bc069df9b12
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-23 22:58:41 +0530

    Update userinfo.py

--- NekoRobot/modules/userinfo.py
+++ NekoRobot/modules/userinfo.py
@@ -160,7 +160,7 @@ def get_id(update: Update, context: CallbackContext):
             )
 
 
-@NekoTelethonClient.on(
+@telethn.on(
     events.NewMessage(
         pattern="/ginfo ", from_users=(TIGERS or []) + (DRAGONS or []) + (DEMONS or [])
     )
