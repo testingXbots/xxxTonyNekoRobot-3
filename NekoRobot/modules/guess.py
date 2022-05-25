4d4558f56aea21875c958ca397d086a15eba0a51
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-25 12:38:41 +0530

    Update guess.py

--- NekoRobot/modules/guess.py
+++ NekoRobot/modules/guess.py
@@ -8,9 +8,9 @@ async def wish(e):
  if e.is_reply:
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
-         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         fire = "https://telegra.ph/file/9ff2283cd3aea55c49c5f.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
-         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         fire = "https://telegra.ph/file/9ff2283cd3aea55c49c5f.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=e)
