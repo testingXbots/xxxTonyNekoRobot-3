1928e118b8961bd1699325ebd5d7f0fe3c9c806f
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-05-27 14:00:39 +0530

    Update chatbot.py

--- NekoRobot/modules/chatbot.py
+++ NekoRobot/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "**Ⲥⲏʟⲟⲉ** chatbot disabled by {}.".format(mention_html(user.id, user.first_name)),
+                "Ⲥⲏʟⲟⲉ chatbot disabled by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "**Ⲥⲏʟⲟⲉ** chatbot enabled by {}.".format(mention_html(user.id, user.first_name)),
+                "Ⲥⲏʟⲟⲉ chatbot enabled by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
