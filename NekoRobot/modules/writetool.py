df3c26e62562bd51ea3ef948c58b5d8646d164ac
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-06-02 17:09:36 +0530

    Update writetool.py

--- NekoRobot/modules/writetool.py
+++ NekoRobot/modules/writetool.py
@@ -7,14 +7,14 @@ from NekoRobot import pbot
 async def handwriting(_, message):
     if len(message.command) < 2:
         return await message.reply_text("» Give some text to write it on my copy...")
-    m = await message.reply_text("» wait a sec, let me write that text...")
+    m = await message.reply_text("`wait a sec, let me write that text...`")
     name = (
         message.text.split(None, 1)[1]
         if len(message.command) < 3
         else message.text.split(None, 1)[1].replace(" ", "%20")
     )
     hand = "https://apis.xditya.me/write?text=" + name
-    await m.edit("`Processing...`")
+    await m.edit("`Uploading...`")
     await pbot.send_chat_action(message.chat.id, "upload_photo")
     await message.reply_photo(hand, caption="**Written with🖊 by [Ⲥⲏʟⲟⲉ](t.me/ChloeXRobot)**")
     await m.delete()
