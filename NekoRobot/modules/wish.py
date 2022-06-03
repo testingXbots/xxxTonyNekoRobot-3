445e0cb2922aedbe0d902b052089365159bb54fa
RubySpideybot <86848301+RubySpideybot@users.noreply.github.com>
2022-06-03 11:51:06 +0530

    Update wish.py

--- NekoRobot/modules/wish.py
+++ NekoRobot/modules/wish.py
@@ -8,9 +8,9 @@ async def wish(e):
  if e.is_reply:
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
-         fire = "https://telegra.ph/file/9ff2283cd3aea55c49c5f.jpg"
+         fire = "https://telegra.ph/file/85f91175ae99287d54cc4.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__Chance of success {mm}%__", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
-         fire = "https://telegra.ph/file/9ff2283cd3aea55c49c5f.jpg"
+         fire = "https://telegra.ph/file/85f91175ae99287d54cc4.jpg"
          await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__Chance of success {mm}%__", reply_to=e)
